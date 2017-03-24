601301c0f235c479fa6d13229604cb6eda24a6be
Guiliano <test@pro.ru>
2017-03-25 01:12:26 +0400

    Внесены мелкие корректировки

--- blog/modules/admin/views/article/index.php
+++ blog/modules/admin/views/article/index.php
@@ -19,8 +19,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
             'id',
             'title',
             'description:ntext',
