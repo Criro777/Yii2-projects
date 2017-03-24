a801dd981e8abb121800d782036e891299985b9b
Guiliano <test@pro.ru>
2017-03-25 01:11:37 +0400

    Внесены корректировки в шаблон админки и форму редактирования статей, добавлен плагин select2

--- blog/modules/admin/views/article/_form.php
+++ blog/modules/admin/views/article/_form.php
@@ -1,5 +1,8 @@
 <?php
 
+use app\models\Category;
+use app\models\Tag;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -14,15 +17,24 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
+    <div class="form-group">
+        <label class="control-label" for="category">Категория</label>
+        <?= Html::dropDownList('category', $model->category, ArrayHelper::map(Category::find()->all(), 'id', 'title'), ['class' => 'form-control select_cat']) ?>
+    </div>
+
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
 
+    <div class="form-group">
+        <label class="control-label" for="tags">Теги</label>
+
+        <?= Html::dropDownList('tags', ArrayHelper::getColumn($model->getTags()->select('id')->asArray()->all(), 'id'), ArrayHelper::map(Tag::find()->all(), 'id', 'title'), ['class' => 'form-control select2', 'multiple'=>true]) ?>
+    </div>
+
     <?= $form->field($model, 'image')->fileInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'date')->textInput() ?>
-
-    <?= $form->field($model, 'category_id')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Создать' : 'Обновить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
--- blog/views/layouts/admin.php
+++ blog/views/layouts/admin.php
@@ -84,6 +84,9 @@ AppAsset::register($this);
     })
 
 </script>
+<script type="text/javascript">
+    $('.select2').select2();
+</script>
 </body>
 </html>
 <?php $this->endPage() ?>
